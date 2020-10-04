a5fbebc15198af80f0fdd6f339ec18e772b72567
Calandrinon <georgesut@yahoo.com>
2020-10-04 12:55:49 +0300

    Add simple communication

--- Lab-1/spaces_in_string/client.c
+++ Lab-1/spaces_in_string/client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -6,6 +7,7 @@
 #include <arpa/inet.h>
 #include <netinet/ip.h>
 #include <netinet/in.h>
+#define BUF_LEN 1024
 
 // A client sends to the server a string. The server returns the count of spaces in the string.
 
@@ -15,7 +17,7 @@ int main() {
 	socket_structure.sin_port = htons(9999);
 	inet_aton("192.168.1.13", &socket_structure.sin_addr);
 	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	
+
 	if (socket_fd < 0) {
 		printf("Error: Socket could not be created\n");	
 		return 0;
@@ -25,16 +27,11 @@ int main() {
 		printf("Error: Could not establish a connection to the server.\n");	
 		return 0;
 	}
-	
-	char string_request[50];
-	// First, we have to send the size of the string.
-	printf("Send a string to the server and receive the number of spaces in the string.\n");
-	printf("Enter a string: ");
-	fgets(string_request, 50, stdin);
-	send(socket_fd, string_request, sizeof(string_request), 0);	
 
-	int number_of_spaces;
-	recv(socket_fd, &number_of_spaces, sizeof(number_of_spaces), 0);	
-	printf("The number of spaces is %d\n", number_of_spaces);
+	/// This is currently just a test to see if sending and receiving a string over a network works.	
+	char message[BUF_LEN];
+	recv(socket_fd, message, BUF_LEN, 0);	
+	printf("%s\n", message);
+	close(socket_fd);
 	return 0;
 }
---
+++ Lab-1/spaces_in_string/server.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#ifndef WIN32
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/ip.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#else
+#include <WinSock2.h>
+#include <stdint.h>
+#endif
+
+#define BUF_LEN 1024
+int main() {
+
+    #ifdef WIN32
+        WSADATA wsaData;
+        if (WSAStartup() < 0) {
+            printf("Could not initialize WinSock API.\n");
+            return 0;
+        }
+    #endif
+
+    struct sockaddr_in socket_str;
+    socket_str.sin_family = AF_INET;
+    socket_str.sin_port = htons(9999);
+    socket_str.sin_addr.s_addr = INADDR_ANY;
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (socket_fd < 0) {
+        printf("Could not create socket...\n");
+        return 0;
+    }
+
+    if (bind(socket_fd, (struct sockaddr*)&socket_str, sizeof(socket_str)) < 0) {
+        printf("Could not bind socket to address and port...\n");
+        return 0;
+    }
+
+    if (listen(socket_fd, 5) < 0) {
+        printf("Could not listen on the specified port and address\n");
+        return 0;
+    }
+    printf("Listening...\n");
+
+    while (true) {
+        struct sockaddr_in client_address;
+        int address_size = sizeof(client_address);
+        int client_connection = accept(socket_fd, (struct sockaddr*)&client_address, &address_size);
+
+        if (client_connection < 0) {
+            printf("Cannot connect with the client!\n");
+            continue;
+        } 
+
+        printf("Connected with a client.\n");
+
+        char message[BUF_LEN];
+        strcpy(message, "Hello, stranger!");
+        send(client_connection, message, BUF_LEN, 0);
+
+        #ifdef WIN32
+            closesocket(client_connection);
+        #else
+            close(client_connection);
+        #endif
+    }
+
+    #ifdef WIN32
+        closesocket(socket_fd);
+        WSACleanup();
+    #else
+        close(socket_fd);
+    #endif
+    return 0;
+}
